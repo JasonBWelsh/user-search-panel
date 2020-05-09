d7417998520af85b510d269bcf4ae7e6968d2932
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-09 11:57:06 -0400

    recent item UI change

--- src/components/RecentItem/styles.js
+++ src/components/RecentItem/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledRecentItem = styled.div`
   margin-right: 5px;
   margin-bottom: 5px;
-  background: #769976;
+  background: #aeaeae;
   color: #fff;
   border-radius: 3px;
   font-size: 14px;
